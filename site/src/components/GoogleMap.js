0a8e2db07d864c4f0158d07d1dad0b22a90199cf
Larry Hudson <larryhudson@gmail.com>
2019-03-18 19:24:06 +1100

    trying to fix google map rendering

--- site/src/components/GoogleMap.js
+++ site/src/components/GoogleMap.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import GoogleMapReact from 'google-map-react';
 
+
 const Wrapper = styled.main`
   width: 100%;
   height: 100%;
