9c78f80c023b91e516ec13cdea7155115547fe84
Larry Hudson <larryhudson@gmail.com>
2019-03-25 08:40:46 +1100

    trying to fix day props bug

---
+++ site/src/components/GenreEventsList.js
@@ -0,0 +1,93 @@
+import PropTypes from "prop-types"
+import React from "react"
+import {getGenreName, genreColour} from "../consts/genres"
+import styled from 'styled-components'
+import { space } from 'styled-system'
+import { Link } from 'gatsby'
+
+const EventCard = styled(Link)`
+  background: ${props => props.bg ? props.bg : 'lightgray'};
+  list-style-type: none;
+  flex-basis: calc(50% - 5px);
+  font-size: 90%;
+  padding: 5px;
+  color: black;
+  margin-bottom: 10px;
+  text-decoration: none;
+
+@media (min-width: 480px) {
+  flex-basis: calc(33.33% - 5px);
+  padding: 10px;
+  font-size: 100%;
+}
+
+@media (min-width: 768px) {
+  flex-basis: calc(25% - 5px);
+}
+`
+
+const CardList = styled.div`
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: space-between;
+`
+
+const EventTitle = styled.h4`
+margin-bottom: 0.5em;
+line-height: 1.5;
+font-size: 90%;
+
+@media (min-width: 480px) {
+  font-size: 100%;
+}
+`
+
+const EventVenue = styled.span`
+`
+
+const GenreHeading = styled.h3`
+background: ${props => props.bg ? props.bg : 'lightgray'};
+${space}
+`
+
+class GenreEventsList extends React.Component {
+	
+  render() {
+
+	  const {genres, date} = this.props
+	  return (
+	  	<div>
+	      {genres.map( genre => (
+	      <div key={'genre-events-' + date + genre.fieldValue}>
+	      <GenreHeading p={['2','3']} id={genre.fieldValue} bg={genreColour(genre.fieldValue, 0.25)}>{getGenreName(genre.fieldValue)} ({genre.totalCount})</GenreHeading>
+	      <CardList>
+	      {genre.edges.map( ({node: event}) => {
+		        return <EventCard key={event.slug} to={event.slug} color='black' bg={genreColour(event.genre, 0.25)}>
+		        <EventTitle>{event.mainArtist ? event.mainArtist.name : event.title}</EventTitle>
+		        <EventVenue>{event.venue.name}</EventVenue><br />
+		        {event.startTime && event.startTime}<br />
+		        {event.price && (
+		          (event.price === '$0.00') ? 'Free' : event.price
+		        )}
+		        </EventCard>
+	        })}
+	      </CardList>
+	      </div>
+	      ))}
+	  </div>
+	  )
+	}
+}
+
+GenreEventsList.propTypes = {
+  genres: PropTypes.array,
+  date: PropTypes.string
+}
+
+GenreEventsList.defaultProps = {
+  genres: [],
+  date: '',
+}
+
+export default GenreEventsList
--- site/src/templates/day.js
+++ site/src/templates/day.js
@@ -6,6 +6,7 @@ import MainMap from "../components/MainMap"
 import {getGenreName, genreColour} from "../consts/genres"
 import styled from 'styled-components'
 import { space } from 'styled-system'
+import GenreEventsList from '../components/GenreEventsList'
 
 function sortById(a,b) {
   if (a.fieldValue < b.fieldValue)
@@ -38,53 +39,6 @@ label {
   display: block;
 }
 ${space}
-`
-
-
-const GenreHeading = styled.h3`
-background: ${props => props.bg ? props.bg : 'lightgray'};
-${space}
-`
-
-const EventCard = styled(Link)`
-  background: ${props => props.bg ? props.bg : 'lightgray'};
-  list-style-type: none;
-  flex-basis: calc(50% - 5px);
-  font-size: 90%;
-  padding: 5px;
-  color: black;
-  margin-bottom: 10px;
-  text-decoration: none;
-
-@media (min-width: 480px) {
-  flex-basis: calc(33.33% - 5px);
-  padding: 10px;
-  font-size: 100%;
-}
-
-@media (min-width: 768px) {
-  flex-basis: calc(25% - 5px);
-}
-`
-
-const CardList = styled.div`
-display: flex;
-flex-direction: row;
-flex-wrap: wrap;
-justify-content: space-between;
-`
-
-const EventTitle = styled.h4`
-margin-bottom: 0.5em;
-line-height: 1.5;
-font-size: 90%;
-
-@media (min-width: 480px) {
-  font-size: 100%;
-}
-`
-
-const EventVenue = styled.span`
 `
 
 class DayPage extends React.Component {
@@ -170,6 +124,7 @@ class DayPage extends React.Component {
       }
       )}
     </CheckboxList>
+    <GenreEventsList genres={showingGenres} date={date} />
     {/*<h3>Jump to genre</h3>
     {genres.map( genre => (
       <GenreHeading p={['2','3']} key={'jump-to-' + genre.fieldValue}
@@ -180,23 +135,6 @@ class DayPage extends React.Component {
       </GenreHeading>
     ))}
   */}
-    {showingGenres.map( genre => (
-      <div key={'genre-events-' + date + genre.fieldValue}>
-      <GenreHeading p={['2','3']} id={genre.fieldValue} bg={genreColour(genre.fieldValue, 0.25)}>{getGenreName(genre.fieldValue)} ({genre.totalCount})</GenreHeading>
-      <CardList>
-      {genre.edges.map( ({node: event}) => {
-        return <EventCard key={event.slug} to={event.slug} color='black' bg={genreColour(event.genre, 0.25)}>
-        <EventTitle>{event.mainArtist ? event.mainArtist.name : event.title}</EventTitle>
-        <EventVenue>{event.venue.name}</EventVenue><br />
-        {event.startTime && event.startTime}<br />
-        {event.price && (
-          (event.price === '$0.00') ? 'Free' : event.price
-        )}
-        </EventCard>
-        })}
-      </CardList>
-      </div>
-      ))}
     </Layout>
   )}
 
